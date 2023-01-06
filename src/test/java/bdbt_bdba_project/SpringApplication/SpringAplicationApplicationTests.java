c86b5adaec56f6b95ba92675d622bdf5053cdea5
pokrasa <01169332@pw.edu.pl>
2023-01-06 17:55:27 +0100

    aa

---
+++ src/test/java/bdbt_bdba_project/SpringApplication/SpringAplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package bdbt_bdba_project.SpringApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
